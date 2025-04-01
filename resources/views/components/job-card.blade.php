0d3fa58e95742eeb112075388c2a61d3b6cec65a
Elvis Maki <elvis-maki@hotmail.co.uk>
2025-04-01 13:50:17 +0100

    Add initial project structure with configuration files, routes, and basic components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <section>
+        <h3 class="mb-6">Top Jobs</h3>
+
+        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+            <div class="self-start text-sm">PHP Developer</div>
+
+                <div clsdd="py-8 font-bold">
+                    <h3>Front End</h3>
+                    <p>Full Time - £60,000</p>
+                </div>
+
+                <div class="flex justify-between items-center mt-auto">
+                    <div>
+                        <x-tag>Tag</x-tag>
+                        <x-tag>Tag</x-tag>
+                        <x-tag>Tag</x-tag>
+                    </div>
+
+                    <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
