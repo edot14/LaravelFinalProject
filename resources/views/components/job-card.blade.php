a443ab50ee9e34694bf6bcd30747d31c79c75c57
Elvis Maki <elvis-maki@hotmail.co.uk>
2025-04-06 19:03:09 +0100

    Add initial models, controllers, and views for job and employer management - On going

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,18 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">PHP Developer</div>
 
-    <div clsdd="py-8 font-bold">
-        <h3>Front End</h3>
-        <p>Full Time - £60,000</p>
+    <div clsdd="py-8">
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300 mt-3">Front End</h3>
+        <p class="text-sm mt-4">Full Time - £60,000</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Front-End</x-tag>
+            <x-tag size="small">Back-End</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
 
-        <img src="https://placehold.jp/3d4070/ffffff/42x42.jpg" alt="" class="rounded-xl">
+        <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
         extend: {
             colors: {
                 "black": "#060606"
+            },
+            fontFamily: {
+                "roboto": ["Roboto", "sans-serif"]
+            },
+            fontSize: {
+                "2xs": ".625rem" //10px divded by the base font size which is 16 = 0.625rem.
             }
         },
     },
