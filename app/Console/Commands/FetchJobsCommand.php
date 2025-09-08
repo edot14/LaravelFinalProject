7c48c6893b82b1ff50a9256ba4f3a1930bc0eeff
elvis maki <elvis@remarco.co.uk>
2025-09-09 00:07:57 +0100

    ToDo - Ongoing development on Fetch, need to sort the .env to work with Table Plus

--- app/Console/Commands/FetchJobsCommand.php
+++ app/Console/Commands/FetchJobsCommand.php
@@ -7,6 +7,8 @@ use App\Services\JobFetcherService;
 
 class FetchJobsCommand extends Command
 {
+
+    // TODO-Sort out your apis for it to work on table plus properly
     protected $signature = 'jobs:fetch';
     protected $description = 'Fetch jobs from API and update the database';
 
