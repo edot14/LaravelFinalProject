b65d69ca1e1b09e73e92e6ca266adcfcfc56d4b5
elvis maki <elvis@remarco.co.uk>
2025-11-06 10:29:00 +0000

    Change on text error for the Schedule select for it to appear properly.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -7,16 +7,16 @@
         <x-forms.input label="Location" name="location" placeholder="Victoria, London" />
 
         <x-forms.select label="Schedule" name="schedule">
-            <option>Part Time</option>
-            <option>Full Time</option>
+            <option class="text-black">Part Time</option>
+            <option class="text-black">Full Time</option>
         </x-forms.select>
 
-        <x-forms.input label="URL" name="url" placeholder="https://example.com/jobs/" />
-        <x-forms.checkbox label="Feature (Costs Extra)" name="featured" />
+        <!--<x-forms.input label="URL" name="url" placeholder="https://example.com/jobs/" />
+        <x-forms.checkbox label="Feature (Costs Extra)" name="featured" />-->
 
         <x-forms.divider />
 
-        <x-forms.input label="Tags (comma separated)" name="tags" placeholder="laracasts, video, education" />
+        <x-forms.input label="Tags (comma separated)" name="tags" placeholder="London, Remote, Education" />
 
         <x-forms.button>Publish</x-forms.button>
     </x-forms.form>
