ed6ba8956f9a6016d97f5cc5243410fa563e3335
elvis maki <elvis@remarco.co.uk>
2025-11-12 09:37:43 +0000

    Development on Companies page layout with a section to view Jobs.

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
-    <h1 class="text-2xl font-bold">Companies Page</h1>
+    <section class="py-20 bg-gradient-to-r from-neutral-950 via-neutral-900 to-neutral-950">
+        <div class="container px-4 mx-auto">
+            <div class="max-w-2xl mx-auto">
+                <div class="relative mb-12">
+                    <div
+                        class="absolute inset-0 opacity-20 bg-gradient-to-r from-purple-800 to-purple-950 rounded-2xl filter blur-3xl">
+                    </div>
+                    <h2
+                        class="text-3xl font-medium leading-tight text-transparent md:text-4xl lg:text-5xl bg-clip-text bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300">
+                        Explore Top Companies
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!--
+    <section class="py-20">
+        <div class="container px-4 mx-auto">
+            <x-section-heading>Available Jobs</x-section-heading>
+            <div class="grid gap-8 mt-6 lg:grid-cols-3">
+
+            </div>
+        </div>
+    </section>
+    -->
 </x-layout>
